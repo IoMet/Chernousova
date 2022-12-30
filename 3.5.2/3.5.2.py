f953ef991b332b2f27af58a1fa5fee276c532f34
IoMet <chas.n3@mail.ru>
2022-12-30 13:45:59 +0500

    Задание 3.5.3

--- 3.5.2/3.5.2.py
+++ 3.5.2/3.5.2.py
@@ -1,51 +1,51 @@
-import math
-from statistics import mean
-import pandas as pd
-import sqlite3
-
-
-def set_salary_with_cur(salary_from, salary_to, currency, published_at):
-    published_at = published_at[1] + "/" + published_at[0]
-    currency_value = 0
-
-    if currency != "RUR" and (currency == currency):
-        if currency in ["BYN", "BYR", "EUR", "KZT", "UAH", "USD"]:
-            currency = "BYR" if currency == "BYN" else currency
-            cursor.execute("SELECT * FROM currency_from_api "
-                           "WHERE date == :published_at", {"published_at": published_at})
-            currency_value = cursor.fetchall()[0][currency_dict[currency]]
-    elif currency == "RUR":
-        currency_value = 1
-
-    return check_conditions(salary_from, salary_to, currency_value)
-
-
-def check_conditions(salary_from, salary_to, currency_value):
-    from_nan = math.isnan(salary_from)
-    to_nan = math.isnan(salary_to)
-
-    if from_nan and not to_nan:
-        return salary_to * currency_value
-    elif not from_nan and to_nan:
-        return salary_from * currency_value
-    elif not from_nan and not to_nan:
-        return mean([salary_from, salary_to]) * currency_value
-
-
-df = pd.read_csv("vdc.csv")
-connection = sqlite3.connect("currency_from_api.db")
-cursor = connection.cursor()
-currency_dict = {"BYR": 1, "USD": 2, "EUR": 3, "KZT": 4, "UAH": 5}
-
-df["years"] = df["published_at"].apply(lambda date: date[:4])
-df["salary"] = df.apply(
-    lambda row: set_salary_with_cur(row["salary_from"], row["salary_to"], row["salary_currency"],
-                                    row["published_at"][:7].split("-")), axis=1)
-df = df[df["salary"].notnull()]
-
-new_connection = sqlite3.connect("vdc_db.db")
-new_cursor = new_connection.cursor()
-df.to_sql(name="vdc_db", con=new_connection, if_exists='replace', index=False)
-new_connection.commit()
-
-print("Черноусова Анастасия Сергеевна")
+# import math
+# from statistics import mean
+# import pandas as pd
+# import sqlite3
+#
+#
+# def set_salary_with_cur(salary_from, salary_to, currency, published_at):
+#     published_at = published_at[1] + "/" + published_at[0]
+#     currency_value = 0
+#
+#     if currency != "RUR" and (currency == currency):
+#         if currency in ["BYN", "BYR", "EUR", "KZT", "UAH", "USD"]:
+#             currency = "BYR" if currency == "BYN" else currency
+#             cursor.execute("SELECT * FROM currency_from_api "
+#                            "WHERE date == :published_at", {"published_at": published_at})
+#             currency_value = cursor.fetchall()[0][currency_dict[currency]]
+#     elif currency == "RUR":
+#         currency_value = 1
+#
+#     return check_conditions(salary_from, salary_to, currency_value)
+#
+#
+# def check_conditions(salary_from, salary_to, currency_value):
+#     from_nan = math.isnan(salary_from)
+#     to_nan = math.isnan(salary_to)
+#
+#     if from_nan and not to_nan:
+#         return salary_to * currency_value
+#     elif not from_nan and to_nan:
+#         return salary_from * currency_value
+#     elif not from_nan and not to_nan:
+#         return mean([salary_from, salary_to]) * currency_value
+#
+#
+# df = pd.read_csv("vdc.csv")
+# connection = sqlite3.connect("currency_from_api.db")
+# cursor = connection.cursor()
+# currency_dict = {"BYR": 1, "USD": 2, "EUR": 3, "KZT": 4, "UAH": 5}
+#
+# df["years"] = df["published_at"].apply(lambda date: date[:4])
+# df["salary"] = df.apply(
+#     lambda row: set_salary_with_cur(row["salary_from"], row["salary_to"], row["salary_currency"],
+#                                     row["published_at"][:7].split("-")), axis=1)
+# df = df[df["salary"].notnull()]
+#
+# new_connection = sqlite3.connect("vdc_db.db")
+# new_cursor = new_connection.cursor()
+# df.to_sql(name="vdc_db", con=new_connection, if_exists='replace', index=False)
+# new_connection.commit()
+#
+# print("Черноусова Анастасия Сергеевна")
--- 3.5.3/3.5.3.py
+++ 3.5.3/3.5.3.py
@@ -3,49 +3,38 @@
 # from statistics import mean
 # import pandas as pd
 #
-#
-# def sort_dict(dict_to_sort):
-#     tup = sorted(dict_to_sort.items(), key=lambda item: item[1], reverse=True)[:10]
-#     sorted_dict = {k: v for k, v in tup}
-#     return sorted_dict
-#
-#
 # vacancy_name = input("Введите название профессии: ")
 # vacancy_name = f"%{vacancy_name}%"
 # con = sqlite3.connect("vdc_db.db")
 # cur = con.cursor()
-# database_length = pd.read_sql("SELECT COUNT(*) From new_vac_with_dif_currencies", con).to_dict()["COUNT(*)"][0]
+# file_len = cur.execute("SELECT COUNT(*) From vdc_db").fetchall()[0][0]
 #
-# s_groups_by_y = pd.read_sql("SELECT years, ROUND(AVG(salary)) From new_vac_with_dif_currencies GROUP BY years", con)
-# salaries_by_year = dict(s_groups_by_y[["years", "ROUND(AVG(salary))"]].to_dict("split")["data"])
+# salaries_year = dict(cur.execute("SELECT years, ROUND(AVG(salary)) From vdc_db "
+#                                     "GROUP BY years").fetchall())
 #
-# v_groups_by_y = pd.read_sql("SELECT years, COUNT(name) From new_vac_with_dif_currencies GROUP BY years", con)
-# vacancies_by_year = dict(v_groups_by_y[["years", "COUNT(name)"]].to_dict("split")["data"])
+# vacancies_year = dict(cur.execute("SELECT years, COUNT(name) From vdc_db"
+#                                      " GROUP BY years").fetchall())
 #
-# i_v_s_groups_by_y = pd.read_sql("SELECT years, ROUND(AVG(salary)) From new_vac_with_dif_currencies "
-#                                 "WHERE name LIKE :vacancy_name "
-#                                 "GROUP BY years", con, params=[vacancy_name])
-# inp_vacancy_salary = dict(i_v_s_groups_by_y[["years", "ROUND(AVG(salary))"]].to_dict("split")["data"])
+# vacancy_salary = dict(cur.execute("SELECT years, ROUND(AVG(salary)) From vdc_db "
+#                                       "WHERE name LIKE :vacancy_name "
+#                                       "GROUP BY years", {"vacancy_name": vacancy_name}).fetchall())
 #
-# i_v_c_groups_by_y = pd.read_sql("SELECT years, COUNT(name) From new_vac_with_dif_currencies "
-#                                 "WHERE name LIKE :vacancy_name "
-#                                 "GROUP BY years", con, params=[vacancy_name])
-# inp_vacancy_count = dict(i_v_c_groups_by_y[["years", "COUNT(name)"]].to_dict("split")["data"])
+# vacancy_count = dict(cur.execute("SELECT years, COUNT(name) From vdc_db "
+#                                      "WHERE name LIKE :vacancy_name "
+#                                      "GROUP BY years", {"vacancy_name": vacancy_name}).fetchall())
 #
-# s_a_groups_by_c = pd.read_sql("SELECT area_name, ROUND(AVG(salary)), COUNT(area_name) From new_vac_with_dif_currencies "
-#                               "GROUP BY area_name "
-#                               "ORDER BY COUNT(area_name) DESC ", con)
+# salaries_areas = dict(cur.execute("SELECT area_name, ROUND(AVG(salary)) "
+#                                   "From vdc_db "
+#                                   "GROUP BY area_name "
+#                                   "HAVING COUNT(area_name) >= 0.01 * :file_len "
+#                                   "ORDER BY ROUND(AVG(salary)) DESC", {"file_len": file_len}).fetchall())
 #
-# s_a_groups_by_c = s_a_groups_by_c[s_a_groups_by_c["COUNT(area_name)"] >= 0.01 * database_length]
-# salaries_areas = dict(s_a_groups_by_c[["area_name", "ROUND(AVG(salary))"]].to_dict("split")["data"])
-# salaries_areas = sort_area_dict(salaries_areas)
-#
-# v_a_groups_by_c = pd.read_sql("SELECT area_name, COUNT(area_name) From new_vac_with_dif_currencies "
-#                               "GROUP BY area_name "
-#                               "ORDER BY COUNT(area_name) DESC "
-#                               "LIMIT 10", con)
-# v_a_groups_by_c["COUNT(area_name)"] = round(v_a_groups_by_c["COUNT(area_name)"] / database_length * 100, 2)
-# vacancies_areas = dict(v_a_groups_by_c[["area_name", 'COUNT(area_name)']].to_dict("split")["data"])
+# v_a = pd.read_sql("SELECT area_name, COUNT(area_name) From vdc_db "
+#                   "GROUP BY area_name "
+#                   "ORDER BY COUNT(area_name) DESC "
+#                   "LIMIT 10", con)
+# v_a["COUNT(area_name)"] = round(v_a['COUNT(area_name)'] / file_len * 100, 2)
+# vacancies_areas = dict(v_a[["area_name", 'COUNT(area_name)']].to_dict("split")["data"])
 #
 # print("Динамика уровня зарплат по годам:", salaries_year)
 # print("Динамика количества вакансий по годам:", vacancies_year)
